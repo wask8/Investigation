9e2c9385ab9849d2eb7a4cd77a4b596b770495a2
wask8 <jeremy.charrier@hotmail.fr>
2015-05-16 12:00:48 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+package Investigation;
+
+import javax.swing.ImageIcon;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		fenetreEntree fenet = new fenetreEntree();
+		fenetreOptions fen = new fenetreOptions();
+		fenetreScore fene = new fenetreScore();
+		fenetreEditeur1 fenetr = new fenetreEditeur1();
+		
+		
+	}
+
+}
