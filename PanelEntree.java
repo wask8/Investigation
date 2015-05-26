447bcb7ec4f757824c9dd78c3b71465c66eef734
LucasLEL <lucas.lelievre96@gmail.com>
2015-05-26 15:28:25 +0200

    Update PanelEntree.java

--- PanelEntree.java
+++ PanelEntree.java
@@ -1,4 +1,4 @@
-package mjkl.investigation.display;
+package new_investigation;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -17,12 +17,14 @@ import javax.swing.*;
 public class PanelEntree extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
-	//private Panneau fond = new Panneau();
+	
+	Panneau panneau = new Panneau();
 	private JPanel pan = new JPanel(new BorderLayout());
 	private JLabel texte = new JLabel("Appuyez sur ENTREE pour commencer");
 	
 	public PanelEntree(final Fenetre fen) {
 		//Fenetre globale
+		this.setLayout(null);
 		Dimension dim = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 		int a = (int) dim.getWidth();
 		int b = (int) dim.getHeight();
@@ -34,9 +36,10 @@ public class PanelEntree extends JPanel {
 		pan.setOpaque(false);
 		pan.setBounds(0, 0, a, b);
 		pan.add(texte);
-		pan.setBounds(325, 450, 700, 100);
+		pan.setLayout(null);
+		texte.setBounds((int)(a*425/1366), (int)(b*450/768),(int) (a*700/1366),(int) (b*100/768));
 		texte.setForeground(new Color(180,187,191));
-		Font f2 = new Font("Calibri", Font.PLAIN, 30);
+		Font f2 = new Font("Calibri", Font.PLAIN, a*30/1366);
 		texte.setFont(f2);
 				
 		this.add(pan);
@@ -48,6 +51,8 @@ public class PanelEntree extends JPanel {
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER){
 					fen.setContentPane(new PanelMenu(fen));
+					fen.repaint();
+					fen.revalidate();
 				}
 				
 			}
@@ -66,7 +71,7 @@ public class PanelEntree extends JPanel {
 	
 	public void paintComponent(Graphics g) {
 		try {
-			Image img = ImageIO.read(new File("investigation.jpg"));
+			Image img = ImageIO.read(new File("InvestigationEntree.jpg"));
 			g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
 		} catch (IOException e) {
 			e.printStackTrace();
