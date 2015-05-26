c6c5eded9e79abd7b183e225dc3d0b0103c0cbee
LucasLEL <lucas.lelievre96@gmail.com>
2015-05-26 15:30:27 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+package new_investigation;
+
+import java.awt.CardLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class main {
+
+	public static void main(String[] args) {		
+		Fenetre fen = new Fenetre();
+		
+	}
+
+}
