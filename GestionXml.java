838ab5dc8b36fabc9b8b1c0968463f5f38119ecf
wask8 <jeremy.charrier@hotmail.fr>
2015-05-27 10:50:56 +0200

    Update GestionXml.java

--- GestionXml.java
+++ GestionXml.java
@@ -2,6 +2,8 @@ package investigation;
 
 import java.io.*;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 import org.jdom.*; //ajouter JDOM a la librairie (visiblement pas présent de base)
 import org.jdom.output.*;
 import org.w3c.dom.Element;
@@ -41,4 +43,50 @@ public class GestionXml {
 	      
 	      Element scoreJoueur = new Element("scoreJoueur");
 	      score.addContent(scoreJoueur);
+	      
+	      affiche();
+	      enregistre("data.xml");
+
+	      static void affiche()
+	      {
+	         try
+	         {
+	            //On utilise ici un affichage classique avec getPrettyFormat()
+	            XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+	            sortie.output(document, System.out);
+	         }
+	         catch (java.io.IOException e){}
+	      }
+
+	      static void enregistre(String fichier)
+	      {
+	         try
+	         {
+	            //On utilise ici un affichage classique avec getPrettyFormat()
+	            XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+	            //Il suffit simplement de créer une instance de FileOutputStream
+	            //avec en argument le nom du fichier pour effectuer la sérialisation.
+	            sortie.output(document, new FileOutputStream(fichier));
+	         }
+	         catch (java.io.IOException e){}
+	      }
+	   
+	      static void afficheScore()
+	      {
+	         //On crée une List contenant tous les noeuds "score" de l'Element racine
+	         List listScores = racine.getChildren("score");
+
+	         //On crée un Iterator sur notre liste
+	         Iterator i = listScores.iterator();
+	         while(i.hasNext())
+	         {
+	            //On recrée l'Element courant à chaque tour de boucle afin de
+	            //pouvoir utiliser les méthodes propres aux Element comme :
+	            //sélectionner un nœud fils, modifier du texte, etc...
+	            Element courant = (Element)i.next();
+	            //On affiche le nom de l’élément courant
+	            System.out.println(courant.getChild("pseudo").getText());
+	         }
+	      }
+	   
 }
