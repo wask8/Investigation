614bc342c6b2941d710ed4f19cb4fe785348a23e
LenRenko <kimcharles.rimbault@gmail.com>
2015-05-28 15:51:35 +0200

    Create ChronoPanel.java
    Voici le fichier qui gère le chrono implémenté dans le jeu.

---
+++ ChronoPanel.java
@@ -0,0 +1,59 @@
+package mjkl.investigation.running;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.Timer;
+
+public class ChronoPanel extends JPanel{
+
+	private JButton start = new JButton("Start");
+	private JLabel label1 = new JLabel("Temps restant : 30s");
+	private JProgressBar bar = new JProgressBar();
+	
+	public ChronoPanel(){
+		add(label1);
+		start.addActionListener(new Listener2());
+		add(start);
+		add(bar);
+	}
+	
+	
+	private class Listener2 implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			new SecondListener();
+			
+		}
+	}
+	
+	private class SecondListener implements ActionListener{
+		int seconde=30;
+		Timer timer;
+		private int value = 90;
+		
+		
+		
+		public SecondListener(){
+			timer = new Timer(1000, this);
+			timer.start();
+			bar.setValue(value);
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			seconde--;
+			value-=3;
+			ChronoPanel.this.label1.setText("Temps restant: "+seconde+"s");
+			bar.setValue(value);
+			if(seconde<=0){
+				timer.stop();
+			}
+		}
+	}
+	
+}
