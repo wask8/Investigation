a1c41009811d743901126caf988b13bd4e77375a
LucasLEL <lucas.lelievre96@gmail.com>
2015-05-28 11:59:42 +0200

    Update PanelScore.java

--- PanelScore.java
+++ PanelScore.java
@@ -50,9 +50,9 @@ public class PanelScore extends JPanel {
 		panTitre.setBounds(0, 0, a, (int) (b / 4));
 		panTitre.setOpaque(false);
 		panTitre.add(titre);
-		titre.setBounds((int)(a/1366*470), (int)(b/768*20),(int)(a/1366* 800), (int)(b/768*70));
+		titre.setBounds((int)(a*470/1366), (int)(b*20/768),(int)(a* 800/1366), (int)(b*70/768));
 		panTitre.add(soustitre);
-		soustitre.setBounds((int)(a/1366*515), (int)(b/768*100),(int)(a/1366* 800), (int)(b/768*70));
+		soustitre.setBounds((int)(a*515/1366), (int)(b*100/768),(int)(a* 800/1366), (int)(b*70/768));
 		titre.setFont(f0);
 		titre.setForeground(new Color(180, 187, 191)); // 180,187,191
 		soustitre.setFont(f1);
@@ -60,43 +60,43 @@ public class PanelScore extends JPanel {
 
 		// AJOUT PANEL pantab1 et ELEMENTS
 		pantab1.setLayout(null);
-		pantab1.setBounds(0, (int)(b/768*150), (a / 2) - (int)(a/1366*10), (b / 4) * 3);
+		pantab1.setBounds(0, (int)(b*150/768), (a / 2) - (int)(a*10/1366), b * 3/ 4 );
 		// pantab1.setBackground(Color.orange);
 		pantab1.setOpaque(false);
 		pantab1.add(tab1);
-		tab1.setBounds((int)(a/1366*260), (int)(b/768*10), (int)(a/1366*200), (int)(b/768*50));
+		tab1.setBounds((int)(a*260/1366), (int)(b*10/768), (int)(a*200/1366), (int)(b*50/768));
 		tab1.setFont(f2);
 		tab1.setForeground(new Color(180, 187, 191));
 		pantab1.add(zonetab1);
-		zonetab1.setBounds((int)(a/1366*100),(int)(b/768*100),(int)(a/1366*500),(int)(b/768*300));
+		zonetab1.setBounds((int)(a*100/1366),(int)(b*100/768),(int)(a*500/1366),(int)(b*300/768));
 		zonetab1.setFont(f3);
 
 		// AJOUT PANEL pantab2 et ELEMENTS
 		pantab2.setLayout(null);
-		pantab2.setBounds((a / 2) + (int)(a/1366*10), (int)(b/768*150), a / 2, (b / 4) * 3);
+		pantab2.setBounds((a / 2) + (int)(a*10/1366), (int)(b*150/768), a / 2, (b * 3/ 4) );
 		// pantab2.setBackground(Color.red);
 		pantab2.setOpaque(false);
 		pantab2.add(tab2);
-		tab2.setBounds((int)(a/1366*260), (int)(b/768*10), (int)(a/1366*200), (int)(b/768*50));
+		tab2.setBounds((int)(a*260/1366), (int)(b*10/768), (int)(a*200/1366), (int)(b*50/768));
 		tab2.setFont(f2);
 		tab2.setForeground(new Color(180, 187, 191));
 		pantab2.add(zonetab2);
-		zonetab2.setBounds((int)(a/1366*70), (int)(b/768*100), (int)(a/1366*500), (int)(b/768*300));
+		zonetab2.setBounds((int)(a*70/1366), (int)(b*100/768), (int)(a*500/1366), (int)(b*300/768));
 		zonetab2.setFont(f3);
 
 		// AJOUT PANEL trait
 		trait.setLayout(null);
-		trait.setBounds((a / 2) - (int)(a/1366*10), (int)(b/768*250), (int)(a/1366*3), (int)(b/768*300));
+		trait.setBounds((a / 2) - (int)(a*10/1366), (int)(b*250/768), (int)(a*3/1366), (int)(b*300/768));
 		trait.setBackground(Color.white);
 		trait.setOpaque(true);
 
 		// AJOUT PANEL panbouton ET ELEMENT
 		panbouton.setLayout(null);
-		panbouton.setBounds(0, (int)(b/768*590), (int)(a/1366*1500), (int)(b/768*100));
+		panbouton.setBounds(0, (int)(b*590/768), (int)(a*1500/1366), (int)(b*100/768));
 		panbouton.setOpaque(false);
 		
 		panbouton.add(bouton);
-		bouton.setBounds((int)(a/1366*540),(int)(b/768*10),(int)(a/1366*250),(int)(b/768*50));
+		bouton.setBounds((int)(a*540/1366),(int)(b*10/768),(int)(a*250/1366),(int)(b*50/768));
 		bouton.setFont(f3);
 		bouton.setForeground(new Color(180, 187, 191));
 		bouton.setBackground(Color.darkGray);
