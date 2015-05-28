507acada53b922086a54c921d8cd48802295e34c
LucasLEL <lucas.lelievre96@gmail.com>
2015-05-28 11:43:58 +0200

    Update PanelArcade.java

--- PanelArcade.java
+++ PanelArcade.java
@@ -30,6 +30,10 @@ public class PanelArcade extends JPanel {
 	private JPanel panTemps = new JPanel();
 	private JLabel titre = new JLabel("Investigation");
 	private JButton abandon = new JButton("Abandonner la partie");
+	private JLabel labelQuestion = new JLabel("<HTML>Question<br/>données XML</HTML>");
+	private JLabel temps = new JLabel("<HTML>Temps restant<br/>Fonction chrono</HTML>");
+	private JLabel score = new JLabel("<HTML>Score<br/>données XML</HTML>");
+	private JLabel zoneLieu = new JLabel("Lieu à placer : XML");
 
 	public PanelArcade(final Fenetre fen) {
 		// FENETRE GLOBALE
@@ -87,7 +91,7 @@ public class PanelArcade extends JPanel {
 				b * 620 / 768); 
 		panCarte.setBackground(Color.red);
 		JLabel label = new JLabel();
-		label.setIcon(new ImageIcon("Map_Laval,_Mayenne.jpg"));
+		label.setIcon(new ImageIcon("planIUT2.jpg"));
 		panCarte.add(label);
 		label.setBounds(a * 200 / 1366, b * 10 / 768, a * 700 / 1366,
 				b * 671 / 768);
@@ -97,24 +101,38 @@ public class PanelArcade extends JPanel {
 		panScore.setOpaque(true);
 		panScore.setBounds(a * 1100 / 1366, b * 300 / 768, a * 200 / 1366,
 				b * 120 / 768);
+		panScore.add(score);
+		score.setFont(f2);
+		score.setBounds(a * 10/1366, b*5/768, a*200/1366, b*120/768);
 
 		// AJOUT PANEL panQuestion ET ELEMENTS
 		panQuestion.setLayout(null);
 		panQuestion.setOpaque(true);
 		panQuestion.setBounds(a * 1100 / 1366, b * 500 / 768, a * 200 / 1366,
 				b * 120 / 768); 
+		panQuestion.add(labelQuestion);
+		labelQuestion.setFont(f2);
+		labelQuestion.setBounds(a * 5 / 1366, b * 5 / 768, a * 200 / 1366,
+				b * 120 / 768);
 
 		// AJOUT PANEL panLieu ET ELEMENTS
 		panLieu.setLayout(null);
 		panLieu.setOpaque(true);
 		panLieu.setBounds(a * 150 / 1366, b * 25 / 768, a * 800 / 1366,
 				b * 75 / 768); 
+		panLieu.add(zoneLieu);
+		zoneLieu.setFont(f2);
+		zoneLieu.setBounds(a*10/1366,b*10/1366,a*800/1366,b*75/768);
 		
 		// AJOUT PANEL panTemps ET ELEMENTS
 		panTemps.setLayout(null);
 		panTemps.setOpaque(true);
 		panTemps.setBounds(a * 1100 / 1366, b * 150 / 768, a * 200 / 1366,
-				b * 70 / 768); // AJOUTER RATIO DE PROPORTION
+				b * 70 / 768); 
+		panTemps.add(temps);
+		temps.setFont(f2);
+		temps.setBounds(a * 0 / 1366, b * 0 / 768, a * 200 / 1366,
+				b * 70 / 768);
 
 		this.add(panEntete);
 		this.add(panCarte);
