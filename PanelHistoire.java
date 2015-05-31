858d3ce5a4c7e7faf30e59e5dbee2a9745063958
LucasLEL <lucas.lelievre96@gmail.com>
2015-05-31 15:59:14 +0200

    Update PanelHistoire.java

--- PanelHistoire.java
+++ PanelHistoire.java
@@ -14,8 +14,27 @@ import java.text.AttributedCharacterIterator;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class PanelHistoire extends JPanel implements KeyListener {
+public class PanelHistoire extends JPanel {
 	
+	int i = 0, question = 0;
+	String parties[] = {
+			"<HTML><CENTER>Assistance Eclipse<BR/>Bonjour jeune stagiaire, la police veut que vous vous rendiez en Mayenne, plus précisément sur Laval.<BR/>Préparez-vous, et rendez vous sur Laval et je vous recontacterai une fois arrivé.<BR/> PLACEZ LAVAL</CENTER><HTML>",
+			"<HTML><CENTER>Assistante Eclipse<BR/>Bien, nous y voilà, l’équipe de police nous attend sur la place du 11 Novembre.<BR/>PLACEZ LA PLACE DU 11 NOVEMBRE</CENTER></HTML>",
+			"<HTML><CENTER>Capitaine Python<BR/>C’est donc vous l’inspecteur ____ ? Bien, sans plus tarder, nous allons nous diriger sur les lieux du crime.<BR/>Vous allez constater les faits par vous-même. Le crime a eu lieu à l’Institut Universitaire Technologique.<BR/>PLACEZ L'IUT</CENTER></HTML>",
+			"<HTML><CENTER>Capitaine Python<BR/>Un témoin a vu une personne s’enfuir en courant. Il semblerait qu’il se dirigeait vers le département informatique.<BR/>PLACEZ LE DEPARTEMENT INFORMATIQUE</CENTER></HTML>",
+			"<HTML><CENTER>Assistante Eclipse<BR/>Il y a de la lumière dans l’une des salles au premier étage, c’est étrange... Il s’agit de la salle Gotham.<BR/>PLACEZ LA SALLE GOTHAM</CENTER><HTML>",
+			"<HTML><CENTER>Capitaine Python<BR/><I>(au moment d’entrer dans la salle)</I> Une vitre a été brisée ! Quelqu’un cours en bas !</CENTER></HTML>",
+			"<HTML><CENTER>Assistante Eclipse<BR/><I>(Dehors)</I> Il est parti dans une voiture verte ! Retrouvons-le !",
+			"<HTML><CENTER>Capitaine Python<BR/>Un témoin indique que la voiture se dirige vers la gare ! Vite retrouvons-le.<BR/>PLACEZ LA GARE</CENTER></HTML>",
+			"<HTML><CENTER>Assistante Eclipse<BR/>La voiture n’est plus là ! Il semblerait qu’elle ait quitté la gare il y a peu, elle se dirige vers le théâtre.<BR/>PLACEZ LE THEATRE</CENTER></HTML>",
+			"<HTML><CENTER>Capitaine Python<BR/>Non ce n’était pas ici ! La voiture a été signalé à la sortie de la ville et se dirige vers Evron, il faut le retrouver au plus vite !<BR/>PLACEZ EVRON</CENTER></HTML>",
+			/* Si plus de 2 reponses fausses */"<HTML><CENTER>Assistante Eclipse - C'est trop tard ! Nous avons pris trop de temps pour retrouver sa trace il s'est enfui</CENTER></HTML>",
+			/* Sinon */"<HTML><CENTER>Assistante Eclipse<BR/>Il n’est pas loin ! On y est presque !</CENTER></HTML>",
+			"<HTML><CENTER>Capitaine Python<BR/>Il faut le rattraper ! Il essaye de fuir vers la Manche, ne le laissez pas quitter la Mayenne.<BR/>PLACEZ LE POINT A LA FRONTIERE AVEC LA MANCHE</CENTER></HTML>",
+			"<HTML><CENTER>Capitaine Python<BR/>Ne bouge plus ! <BR/>Vous êtes en état d’arrestation pour avoir débranché le câble Ethernet et pour délit de fuite ! <BR/>C’est un crime impardonnable !</CENTER></HTML>",
+			"<HTML><CENTER>Suspect Luna<BR/>NON NON NON !!  ... Comment avez vous pu me rattraper ?! <BR/>Je me vengerai !</CENTER></HTML>",
+			"<HTML><CENTER>Capitaine Python<BR/>Bien joué détective ______ !<BR/> Vu votre efficacité, vous ne pouvez qu'etre promu dans notre service, vous devenez Sergent !</CENTER></HTML>",
+			"<HTML><CENTER>Assistante Eclipse<BR/>Félicitations _______ !! <BR/>vous avez réussi avec brio votre mission, nous referons appel à vous rapidement.</CENTER></HTML>" };
 	private JPanel panEntete = new JPanel();
 	private JPanel panCarte = new JPanel();
 	private JPanel panScore = new JPanel();
@@ -24,9 +43,10 @@ public class PanelHistoire extends JPanel implements KeyListener {
 	private JPanel panTemps = new JPanel();
 	private JLabel titre = new JLabel("Investigation");
 	private JButton abandon = new JButton("Abandonner la partie");
-	private JLabel label1;
-	private JLabel labelQuestion = new JLabel("<HTML>Question<br/>5/12</HTML>");
+	private JLabel label1 = new JLabel("Appuyer sur entrée pour demarrer le scenario");
+	private JLabel labelQuestion = new JLabel("<HTML><CENTER>Question : <BR/> 0/"+parties.length);
 	private JLabel temps = new JLabel("<HTML>Temps restant</HTML>");
+	
 
 	public PanelHistoire(final Fenetre fen) {
 		// FENETRE GLOBALE
@@ -65,15 +85,6 @@ public class PanelHistoire extends JPanel implements KeyListener {
 			}
 		});
 
-		/*
-		 * abandon.addActionListener(new ActionListener() {
-		 * 
-		 * @Override public void actionPerformed(ActionEvent e) { new Popup(fen,
-		 * a, b);
-		 * 
-		 * }
-		 */
-
 		abandon.setLayout(null);
 		abandon.setOpaque(true);
 		abandon.setBounds(a * 1110 / 1366, b * 20 / 768 , a * 170 / 1366, b * 25 / 768);
@@ -106,21 +117,52 @@ public class PanelHistoire extends JPanel implements KeyListener {
 		labelQuestion.setFont(f2);
 		labelQuestion.setBounds(a * 5 / 1366, b * 5 / 768, a * 200 / 1366,
 				b * 70 / 768);
+		
 
 		// AJOUT PANEL panLeu ET ELEMENTS
+		
 		panLieu.setLayout(null);
 		panLieu.setOpaque(true);
 		panLieu.setBounds(a * 150 / 1366, b * 25 / 768, a * 800 / 1366,
-				b * 75 / 768); // AJOUTER RATIO DE PROPORTION
+				b * 75 / 768); 
 
 		// SCENARIO
-		label1 = new JLabel(
-				"<HTML><I>Appuyer sur entrée pour demarrer le scenario</I></HTML>");
 		panLieu.add(label1);
 		label1.setBounds(a * 15 / 1366, b * 5 / 768, a * 800 / 1366,
 				b * 50 / 768);
-		label1.addKeyListener(this); // KEYLISTENER NON DETECTE 
 		label1.setFont(f2);
+		this.setFocusable(true);
+		this.requestFocus();
+		this.addKeyListener(new KeyListener() {	
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyChar() == KeyEvent.VK_SPACE) {
+					System.out.println("test");
+					label1.setText(parties[i]);
+					i++;
+					labelQuestion.setText(question+"/"+(parties.length-1)+"Questions");
+					question++;
+					
+				}
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			
+		}); 
+		
 
 		// AJOUT PANEL panTemps ET ELEMENTS
 		panTemps.setLayout(null);
@@ -142,55 +184,7 @@ public class PanelHistoire extends JPanel implements KeyListener {
 
 	}
 
-	int i = 0;
-	String parties[] = {
-			"<HTML><CENTER>Assistance Eclipse<BR/>Bonjour jeune stagiaire, la police veut que vous vous rendiez en Mayenne, plus précisément sur Laval.<BR/>Préparez-vous, et rendez vous sur Laval et je vous recontacterai une fois arrivé.<BR/> PLACEZ LAVAL</CENTER><HTML>",
-			"<HTML><CENTER>Assistante Eclipse<BR/>Bien, nous y voilà, l’équipe de police nous attend sur la place du 11 Novembre.<BR/>PLACEZ LA PLACE DU 11 NOVEMBRE</CENTER></HTML>",
-			"<HTML><CENTER>Capitaine Python<BR/>C’est donc vous l’inspecteur ____ ? Bien, sans plus tarder, nous allons nous diriger sur les lieux du crime.<BR/>Vous allez constater les faits par vous-même. Le crime a eu lieu à l’Institut Universitaire Technologique.<BR/>PLACEZ L'IUT</CENTER></HTML>",
-			"<HTML><CENTER>Capitaine Python<BR/>Un témoin a vu une personne s’enfuir en courant. Il semblerait qu’il se dirigeait vers le département informatique.<BR/>PLACEZ LE DEPARTEMENT INFORMATIQUE</CENTER></HTML>",
-			"<HTML><CENTER>Assistante Eclipse<BR/>Il y a de la lumière dans l’une des salles au premier étage, c’est étrange... Il s’agit de la salle Gotham.<BR/>PLACEZ LA SALLE GOTHAM</CENTER><HTML>",
-			"<HTML><CENTER>Capitaine Python<BR/><I>(au moment d’entrer dans la salle)</I> Une vitre a été brisée ! Quelqu’un cours en bas !</CENTER></HTML>",
-			"<HTML><CENTER>Assistante Eclipse<BR/><I>(Dehors)</I> Il est parti dans une voiture verte ! Retrouvons-le !",
-			"<HTML><CENTER>Capitaine Python<BR/>Un témoin indique que la voiture se dirige vers la gare ! Vite retrouvons-le.<BR/>PLACEZ LA GARE</CENTER></HTML>",
-			"<HTML><CENTER>Assistante Eclipse<BR/>La voiture n’est plus là ! Il semblerait qu’elle ait quitté la gare il y a peu, elle se dirige vers le théâtre.<BR/>PLACEZ LE THEATRE</CENTER></HTML>",
-			"<HTML><CENTER>Capitaine Python<BR/>Non ce n’était pas ici ! La voiture a été signalé à la sortie de la ville et se dirige vers Evron, il faut le retrouver au plus vite !<BR/>PLACEZ EVRON</CENTER></HTML>",
-			/* Si plus de 2 reponses fausses */"<HTML><CENTER>Assistante Eclipse - C'est trop tard ! Nous avons pris trop de temps pour retrouver sa trace il s'est enfui</CENTER></HTML>",
-			/* Sinon */"<HTML><CENTER>Assistante Eclipse<BR/>Il n’est pas loin ! On y est presque !</CENTER></HTML>",
-			"<HTML><CENTER>Capitaine Python<BR/>Il faut le rattraper ! Il essaye de fuir vers la Manche, ne le laissez pas quitter la Mayenne.<BR/>PLACEZ LE POINT A LA FRONTIERE AVEC LA MANCHE</CENTER></HTML>",
-			"<HTML><CENTER>Capitaine Python<BR/>Ne bouge plus ! <BR/>Vous êtes en état d’arrestation pour avoir débranché le câble Ethernet et pour délit de fuite ! <BR/>C’est un crime impardonnable !</CENTER></HTML>",
-			"<HTML><CENTER>Suspect Luna<BR/>NON NON NON !!  ... Comment avez vous pu me rattraper ?! <BR/>Je me vengerai !</CENTER></HTML>",
-			"<HTML><CENTER>Capitaine Python<BR/>Bien joué détective ______ !<BR/> Vu votre efficacité, vous ne pouvez qu'etre promu dans notre service, vous devenez Sergent !</CENTER></HTML>",
-			"<HTML><CENTER>Assistante Eclipse<BR/>Félicitations _______ !! <BR/>vous avez réussi avec brio votre mission, nous referons appel à vous rapidement.</CENTER></HTML>" };
+	
 
-	public void paintComponent(Graphics g) {
-		try {
-			Image img = ImageIO.read(new File("investigation.jpg"));
-			g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if (e.getKeyChar() == 10) {
-			System.out.println("test");
-			label1.setText(parties[i]);
-			// Incrémentation (on passe à la partie suivante)
-			i++;
-		}
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 }
