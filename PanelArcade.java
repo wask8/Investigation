a1b4afec2a586961db692e5e377dde2bd846bc3a
LucasLEL <lucas.lelievre96@gmail.com>
2015-06-01 11:05:59 +0200

    Update PanelArcade.java

--- PanelArcade.java
+++ PanelArcade.java
@@ -10,6 +10,10 @@ import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.PaintEvent;
 import java.awt.image.ImageObserver;
 import java.io.File;
@@ -19,14 +23,8 @@ import java.text.AttributedCharacterIterator;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-import mjkl.investigation.main.Chrono;
-import mjkl.investigation.running.ChronoPanel;
-import mjkl.investigation.running.ChronoTime;
-import mjkl.investigation.running.TimerB;
+public class PanelArcade extends JPanel implements MouseListener {
 
-public class PanelArcade extends JPanel {
-
-	private JPanel fond = new JPanel(null);
 	private JPanel panEntete = new JPanel();
 	private JPanel panCarte = new JPanel();
 	private JPanel panScore = new JPanel();
@@ -35,7 +33,10 @@ public class PanelArcade extends JPanel {
 	private JPanel panTemps = new JPanel();
 	private JLabel titre = new JLabel("Investigation");
 	private JButton abandon = new JButton("Abandonner la partie");
-	
+	private JLabel labelQuestion = new JLabel(
+			"<HTML>Question<br/>données XML</HTML>");
+	private JLabel score = new JLabel("<HTML>Score<br/>données XML</HTML>");
+	private JLabel zoneLieu = new JLabel("Lieu à placer");
 	private ChronoPanel chrono = new ChronoPanel();
 
 	public PanelArcade(final Fenetre fen) {
@@ -64,75 +65,133 @@ public class PanelArcade extends JPanel {
 		panEntete.add(abandon);
 		abandon.setLayout(null);
 		abandon.setOpaque(true);
-		abandon.setBounds((int) a / 1366 * 20, (int) b / 768 * 50,
-				(int) a / 1366 * 170, (int) b / 768 * 30);
 		abandon.setFont(f5);
 		abandon.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				new Popup(fen, a, b);
-
+				fen.setContentPane(new PanelMenu(fen));
+				fen.repaint();
+				fen.revalidate();
 			}
 		});
+
+		// @Override
+		// public void actionPerformed(ActionEvent e) {
+		// new Popup(fen, a, b);
+		//
+		// }
+		// });
 		abandon.setLayout(null);
 		abandon.setOpaque(true);
-		abandon.setBounds(1110, 20, 170, 25);
+		abandon.setBounds(a * 1110 / 1366, b * 20 / 768, a * 170 / 1366,
+				b * 25 / 768);
 		abandon.setFont(f5);
 
 		// AJOUT PANEL panCarte ET ELEMENTS
 		panCarte.setLayout(null);
 		panCarte.setOpaque(false);
-		panCarte.setBounds(10, 100, 1050, 620); // AJOUTER RATIO DE PROPORTION
+		panCarte.setBounds(a * 10 / 1366, b * 120 / 768, a * 1050 / 1366,
+				b * 620 / 768);
 		panCarte.setBackground(Color.red);
 		JLabel label = new JLabel();
-		label.setIcon(new ImageIcon("Map_Laval,_Mayenne.jpg"));
+		label.setIcon(new ImageIcon("planIUT2.jpg"));
 		panCarte.add(label);
-		label.setBounds(200, 10, 700, 671);
+		label.setBounds(a * 200 / 1366, b * 10 / 768, a * 700 / 1366,
+				b * 671 / 768);
 
 		// AJOUT PANEL panStat ET ELEMENTS
 		panScore.setLayout(null);
 		panScore.setOpaque(true);
-		panScore.setBackground(Color.red);//trouver l'emplacement du panel.
-		panScore.setBounds(1100, 300, 200, 120); // AJOUTER RATIO DE PROPORTION
+		panScore.setBounds(a * 1100 / 1366, b * 300 / 768, a * 200 / 1366,
+				b * 120 / 768);
+		panScore.add(score);
+		score.setFont(f2);
+		score.setBounds(a * 10 / 1366, b * 5 / 768, a * 200 / 1366,
+				b * 120 / 768);
 
 		// AJOUT PANEL panQuestion ET ELEMENTS
 		panQuestion.setLayout(null);
 		panQuestion.setOpaque(true);
-		panQuestion.setBounds(1100, 500, 200, 120); // AJOUTER RATIO DE
-													// PROPORTION
+		panQuestion.setBounds(a * 1100 / 1366, b * 500 / 768, a * 200 / 1366,
+				b * 120 / 768);
+		panQuestion.add(labelQuestion);
+		labelQuestion.setFont(f2);
+		labelQuestion.setBounds(a * 5 / 1366, b * 5 / 768, a * 200 / 1366,
+				b * 120 / 768);
 
 		// AJOUT PANEL panLieu ET ELEMENTS
 		panLieu.setLayout(null);
 		panLieu.setOpaque(true);
-		panLieu.setBounds(150, 10, 800, 75); // AJOUTER RATIO DE PROPORTION
+		panLieu.setBounds(a * 150 / 1366, b * 25 / 768, a * 800 / 1366,
+				b * 75 / 768);
+		panLieu.add(zoneLieu);
+		zoneLieu.setFont(f2);
+		zoneLieu.setBounds(a * 10 / 1366, b * 10 / 1366, a * 800 / 1366,
+				b * 75 / 768);
+		this.setFocusable(true);
+		this.requestFocus();
+		addMouseListener(this);
 
 		// AJOUT PANEL panTemps ET ELEMENTS
-		panTemps.setOpaque(true);
-		panTemps.setBackground(Color.yellow);
-		panTemps.setBounds(1100, 150, 200, 70); // AJOUTER RATIO DE PROPORTION
+
 		panTemps.add(new JProgressBar());
-		
 		chrono.setOpaque(true);
-		chrono.setBounds(1100,150,200,70);
-		
-		
+		chrono.setBounds(a * 1100 / 1366, b * 150 / 768, a * 200 / 1366,
+				b * 60 / 768);
+
 		this.add(panEntete);
 		this.add(panCarte);
 		this.add(panQuestion);
 		this.add(panScore);
+		this.add(panTemps);
 		this.add(panLieu);
+		this.add(panTemps);
 		this.add(chrono);
 
 	}
 
-	public void paintComponent(Graphics g) {
-		try {
-			Image img = ImageIO.read(new File("investigation.jpg"));
-			g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	String lieu[] = { "LAVAL", "MAYENNE", "EVRON", "ERNEE","CHATEAU GONTIER" };
+	int i = 0;
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+		System.out.println("test");
+		zoneLieu.setText("Lieu à placer : " + lieu[i]);
+		i++;
+		labelQuestion.setText("Question : \n"+i+"/"+lieu.length);
+
 	}
 
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * public void paintComponent(Graphics g) { try { Image img =
+	 * ImageIO.read(new File("investigation.jpg")); g.drawImage(img, 0, 0,
+	 * getWidth(), getHeight(), this); } catch (IOException e) {
+	 * e.printStackTrace(); } }
+	 */
 }
