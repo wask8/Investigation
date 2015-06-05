e73de907506554e1174dc621b59ddce8522c489c
Otaze <t789@outlook.fr>
2015-06-05 12:05:28 +0200

    Update PanelMenu.java

--- PanelMenu.java
+++ PanelMenu.java
@@ -8,6 +8,8 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
@@ -22,6 +24,8 @@ public class PanelMenu extends JPanel{
 
 	
 	private static final long serialVersionUID = 6012171395347130275L;
+	private int a;
+	private int b;
 	private JPanel panBouton = new JPanel();
 	//private JPanel panBouton2 = new JPanel();
 	private JPanel panBtnQuitter = new JPanel();
@@ -37,19 +41,18 @@ public class PanelMenu extends JPanel{
 	private JButton boutonQuitter = new JButton("Quitter");
 	
 	
-	public PanelMenu(final Fenetre fen) {
+	
+	public PanelMenu(Fenetre fen) {
 		//dimension du panel menu
 		
-		int a = (int)(fen.getWidth());
-		int b = (int)(fen.getHeight());
+		 a = (int)(fen.getWidth());
+		 b = (int)(fen.getHeight());
 		this.setPreferredSize(new Dimension(a,b));
 		
-		// fonts et paramètres boutons
+		// fonts et paramï¿½tres boutons
 		Font f0 = new Font("Calibri", Font.PLAIN, a*80/1366);
-		Font f1 = new Font("Calibri", Font.PLAIN, 50);
-		Font f2 = new Font("Calibri", Font.PLAIN, 30);
-		Font f3 = new Font("Calibri", Font.PLAIN, 18);
-		Font f4 = new Font("Calibri", Font.PLAIN, 10);
+		Font f2 = new Font("Calibri", Font.PLAIN, a*18/1366);
+		
 		titre.setFont(f0);
 		titre.setForeground(new Color(180, 187, 191));
 		
@@ -78,7 +81,7 @@ public class PanelMenu extends JPanel{
 		boutonEditeur.setBackground(Color.darkGray);
 		boutonEditeur.setFocusPainted(false);
 		
-		boutonQuitter.setFont(f3);
+		boutonQuitter.setFont(f2);
 		boutonQuitter.setForeground(new Color(180, 187, 191)); // 180,187,191
 		boutonQuitter.setBackground(Color.darkGray);
 		boutonQuitter.setFocusPainted(false);
@@ -86,7 +89,7 @@ public class PanelMenu extends JPanel{
 		//Dispositions des panels
 		this.setLayout(null);
 		int ratiolarg = (int)(a-1.5*a*(0.0732)-1);
-		panEntete.setBounds(0, 0,ratiolarg,(int)(b/3));
+		panEntete.setBounds(0, 0,ratiolarg,(int)(b*2/3));
 		panEntete.setOpaque(false);
 		panEntete.setLayout(null);
 		panBouton.setBounds(0,(int)(b*100/768),ratiolarg,(int)(b));
@@ -98,15 +101,13 @@ public class PanelMenu extends JPanel{
 		
 		
 		//Disposition des Boutons /textes
-		titre.setBounds( (int)(a*470/1366),(int)(b*50/768) ,(int)(b*800/1366),(int)(a*70/768));
+		titre.setBounds( (int)(a*470/1366),(int)(b*50/768) ,(int)(b*900/1366),(int)(a*70/768));
 		boutonHistoire.setBounds((int)(a*375/1366),(int)(b*200/768), (int)(a*200/1366),(int)(b*100/768));
 		boutonArcade.setBounds((int)(a*725/1366),(int)(b*200/768), (int)(a*200/1366),(int)(b*100/768));
 		boutonScore.setBounds((int)(a*580/1366),(int)(b*450/768),(int)(a*150/1366),(int)(b*100/768));
 		boutonEditeur.setBounds((int)(a*950/1366),(int)(b*450/768),(int)(a*150/1366),(int)(b*100/768));
 		boutonOptions.setBounds((int)(a*200/1366),(int)(b*450/768),(int)(a*150/1366),(int)(b*100/768));
-	//	boutonQuitter.setBounds((int)(a*1250/1366),(int)(b*100/768),(int)(a*80/1366),(int)(b*30/768));
-		
-			
+	
 		
 		
 		
@@ -142,8 +143,7 @@ public class PanelMenu extends JPanel{
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				fen.setContentPane(new PanelArcade(fen));
-				fen.repaint();
+				fen.setContentPane(new PanelArcade2(fen));
 				fen.revalidate();
 				
 			}
@@ -154,8 +154,8 @@ public class PanelMenu extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				fen.setContentPane(new PanelHistoire(fen));
-				fen.repaint();
 				fen.revalidate();
+				
 				
 			}
 		});
@@ -165,7 +165,6 @@ public class PanelMenu extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				fen.setContentPane(new PanelOptions(fen));
-				fen.repaint();
 				fen.revalidate();
 				
 			}
@@ -175,7 +174,6 @@ public class PanelMenu extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				fen.setContentPane(new PanelScore(fen));
-				fen.repaint();
 				fen.revalidate();
 				
 			}
@@ -185,11 +183,61 @@ public class PanelMenu extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				fen.setContentPane(new PanelEditeur1(fen));
-				fen.repaint();
 				fen.revalidate();
 				
 			}
 		});
+		
+fen.getRootPane().addComponentListener(new ComponentListener() {
+			
+			@Override
+			public void componentShown(ComponentEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void componentResized(ComponentEvent e) {
+				
+				a= fen.getWidth();
+				b = fen.getHeight();
+				
+				Font f0 = new Font("Calibri", Font.PLAIN, a*80/1366);
+				Font f2 = new Font("Calibri", Font.PLAIN, a*18/1366);
+				
+				titre.setBounds( (int)(a*470/1366),(int)(b*50/768) ,(int)(b*800/1366),(int)(a*70/768));
+				boutonHistoire.setBounds((int)(a*375/1366),(int)(b*200/768), (int)(a*200/1366),(int)(b*100/768));
+				boutonArcade.setBounds((int)(a*725/1366),(int)(b*200/768), (int)(a*200/1366),(int)(b*100/768));
+				boutonScore.setBounds((int)(a*580/1366),(int)(b*450/768),(int)(a*150/1366),(int)(b*100/768));
+				boutonEditeur.setBounds((int)(a*950/1366),(int)(b*450/768),(int)(a*150/1366),(int)(b*100/768));
+				boutonOptions.setBounds((int)(a*200/1366),(int)(b*450/768),(int)(a*150/1366),(int)(b*100/768));
+			
+				boutonHistoire.setFont(f2);
+				boutonArcade.setFont(f2);
+				boutonScore.setFont(f2);
+				boutonOptions.setFont(f2);
+				boutonEditeur.setFont(f2);
+				boutonQuitter.setFont(f2);
+				titre.setFont(f0);
+				
+				
+			fen.revalidate();
+			}
+			
+			@Override
+			public void componentMoved(ComponentEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void componentHidden(ComponentEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		
 		
 	}
 	
