7406cc492eb7f4b8970af9b4563a0c69d63c8cfe
LucasLEL <lucas.lelievre96@gmail.com>
2015-06-06 16:23:53 +0200

    Update popup.java

--- popup.java
+++ popup.java
@@ -18,13 +18,13 @@ public class Popup extends JDialog {
 	private static final long serialVersionUID = -3737769809307992182L;
 	private JPanel fond = new JPanel();
 	private JLabel titre = new JLabel();
-	private JButton quitter = new JButton("Retour");
-	private JButton valider = new JButton("Valider");
+	private JButton quitter = new JButton("Non");
+	private JButton valider = new JButton("Oui");
 	private Popup popup = this;
 	private JPanel pan = new JPanel();
 	private int a = 0;
 	
-	public Popup(Fenetre fen) {
+	public Popup(final Fenetre fen) {
 		a=fen.getWidth();
 		this.setSize((int) (fen.getWidth() / 4), (int) (fen.getHeight() / 4));
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -40,13 +40,13 @@ public class Popup extends JDialog {
 		pan.setOpaque(false);
 
 		titre.setFont(new Font("Calibri", Font.PLAIN, a*18/1366));
-		titre.setText("Voulez-vous vraiment Abandonner?");
+		titre.setText("Voulez-vous vraiment abandonner?");
 		titre.setForeground(new Color(180, 187, 191));
 		
 
 		pan.setBounds(0,(int)(this.getHeight()/3), this.getWidth(), (int)(this.getHeight()/3));
-		valider.setBounds((int) (this.getWidth() * 2 / 3 - 10), (int) (this.getHeight() * 2 / 3 + 20), (int)(fen.getWidth()*75/1200), (int)(fen.getHeight()*30/675));
-		quitter.setBounds((int) (this.getWidth() * 1 / 3 - 10), (int) (this.getHeight() * 2 / 3 + 20),(int)(fen.getWidth()*75/1200), (int)(fen.getHeight()*30/675));
+		valider.setBounds((int) (this.getWidth() * 2 / 3 - 35), (int) (this.getHeight() * 2 / 3 + 20), (int)(fen.getWidth()*75/1200), (int)(fen.getHeight()*30/675));
+		quitter.setBounds((int) (this.getWidth() * 1 / 3 - 35), (int) (this.getHeight() * 2 / 3 + 20),(int)(fen.getWidth()*75/1200), (int)(fen.getHeight()*30/675));
 
 		
 		valider.addActionListener(new ActionListener() {
