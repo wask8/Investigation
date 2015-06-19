daae22e9aeee3eff59fff19c635c4b407d54b1cd
LucasLEL <lucas.lelievre96@gmail.com>
2015-06-19 15:13:54 +0200

    Update PanelChoixCarteArcade.java

--- PanelChoixCarteArcade.java
+++ PanelChoixCarteArcade.java
@@ -13,6 +13,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
@@ -20,6 +22,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import javax.swing.UIDefaults.LazyValue;
 
 public class PanelChoixCarteArcade extends JPanel {
 
@@ -35,17 +38,166 @@ public class PanelChoixCarteArcade extends JPanel {
 	private JButton bouton = new JButton("Retour");
 	private JScrollPane scroll;
 	private PanelChoixCarteArcade choixArcade = this;
+	private Cartes mayenne;
+	private JPanel pan;
+	private Cartes laval;
+	private JPanel pan1;
+	private Cartes iut;
+	private JPanel pan2;
+	private Cartes informatique;
+	private JPanel pan3;
+	
 
 	/* DANS L'ATTENTE D'AUTRE CHOSE */
 	private JButton boutonattente = new JButton("Bouton Provisoire (acces PanelArcade)");
 
-	// private JScrollBar scroll = new JScrollBar();
 
-	public PanelChoixCarteArcade(final Fenetre fen, PanelMenu menu) {
+	public PanelChoixCarteArcade(final Fenetre fen, final PanelMenu menu) {
 
 		a = (int) fen.getWidth();
 		b = (int) fen.getHeight();
-
+		
+		
+		
+		List<Point> listpoint;
+		List<List<Point>> listdelistdepoint = new ArrayList<List<Point>>();
+		
+		listpoint = new ArrayList<Point>();
+		for (int i = 0 ; i < 19; i++){
+			if (i<1){
+				listpoint.add(new Point((int)(a*359/1920),(int)(a*506/1920), true));
+			}
+			else {
+				listpoint.add(new Point((int)(a*(Math.random()*(913-20))/1920),(int)(b*(Math.random()*(815-20))/1080), false));
+			}
+			
+		}
+		listdelistdepoint.add(listpoint);
+		
+		listpoint = new ArrayList<Point>();
+		for (int i = 0 ; i < 19; i++){
+			if (i<1){
+				listpoint.add(new Point((int)(a*359/1920),(int)(a*506/1920), true));
+			}
+			else {
+				listpoint.add(new Point((int)(a*(Math.random()*(913-20))/1920),(int)(b*(Math.random()*(815-20))/1080), false));
+			}
+			
+		}
+		listdelistdepoint.add(listpoint);
+		
+		listpoint = new ArrayList<Point>();
+		for (int i = 0 ; i < 19; i++){
+			if (i<1){
+				listpoint.add(new Point((int)(a*359/1920),(int)(a*506/1920), true));
+			}
+			else {
+				listpoint.add(new Point((int)(a*(Math.random()*(913-20))/1920),(int)(b*(Math.random()*(815-20))/1080), false));
+			}
+			
+		}
+		listdelistdepoint.add(listpoint);
+		
+		listpoint = new ArrayList<Point>();
+		for (int i = 0 ; i < 19; i++){
+			if (i<1){
+				listpoint.add(new Point((int)(a*359/1920),(int)(a*506/1920), true));
+			}
+			else {
+				listpoint.add(new Point((int)(a*(Math.random()*(913-20))/1920),(int)(b*(Math.random()*(815-20))/1080), false));
+			}
+			
+		}
+		listdelistdepoint.add(listpoint);
+		
+		listpoint = new ArrayList<Point>();
+		for (int i = 0 ; i < 19; i++){
+			if (i<1){
+				listpoint.add(new Point((int)(a*359/1920),(int)(a*506/1920), true));
+			}
+			else {
+				listpoint.add(new Point((int)(a*(Math.random()*(913-20))/1920),(int)(b*(Math.random()*(815-20))/1080), false));
+			}
+			
+		}
+		listdelistdepoint.add(listpoint);
+		
+		listpoint = new ArrayList<Point>();
+		for (int i = 0 ; i < 19; i++){
+			if (i<1){
+				listpoint.add(new Point((int)(a*359/1920),(int)(a*506/1920), true));
+			}
+			else {
+				listpoint.add(new Point((int)(a*(Math.random()*(913-20))/1920),(int)(b*(Math.random()*(815-20))/1080), false));
+			}
+			
+		}
+		listdelistdepoint.add(listpoint);
+		
+		listpoint = new ArrayList<Point>();
+		for (int i = 0 ; i < 19; i++){
+			if (i<1){
+				listpoint.add(new Point((int)(a*359/1920),(int)(a*506/1920), true));
+			}
+			else {
+				listpoint.add(new Point((int)(a*(Math.random()*(913-20))/1920),(int)(b*(Math.random()*(815-20))/1080), false));
+			}
+			
+		}
+		listdelistdepoint.add(listpoint);
+		
+		listpoint = new ArrayList<Point>();
+		for (int i = 0 ; i < 19; i++){
+			if (i<1){
+				listpoint.add(new Point((int)(a*359/1920),(int)(a*506/1920), true));
+			}
+			else {
+				listpoint.add(new Point((int)(a*(Math.random()*(913-20))/1920),(int)(b*(Math.random()*(815-20))/1080), false));
+			}
+			
+		}
+		listdelistdepoint.add(listpoint);
+		
+		listpoint = new ArrayList<Point>();
+		for (int i = 0 ; i < 19; i++){
+			if (i<1){
+				listpoint.add(new Point((int)(a*359/1920),(int)(a*506/1920), true));
+			}
+			else {
+				listpoint.add(new Point((int)(a*(Math.random()*(913-20))/1920),(int)(b*(Math.random()*(815-20))/1080), false));
+			}
+			
+		}
+		listdelistdepoint.add(listpoint);
+		
+		listpoint = new ArrayList<Point>();
+		for (int i = 0 ; i < 19; i++){
+			if (i<1){
+				listpoint.add(new Point((int)(a*359/1920),(int)(a*506/1920), true));
+			}
+			else {
+				listpoint.add(new Point((int)(a*(Math.random()*(913-20))/1920),(int)(b*(Math.random()*(815-20))/1080), false));
+			}
+			
+		}
+		listdelistdepoint.add(listpoint);
+		
+		
+		
+		
+		mayenne = new Cartes(new Dimension( a * 650 / 1366, b * 580 / 768), fen, getClass().getResource("Mayenne.png"), "Mayenne", listdelistdepoint, null);
+		pan = mayenne.getMiniature();
+		
+		laval = new Cartes(new Dimension( a * 650 / 1366, b * 580 / 768), fen, getClass().getResource("Laval.jpg"), "Laval", listdelistdepoint, null);
+		pan1 = laval.getMiniature();
+		
+		iut = new Cartes(new Dimension( a * 650 / 1366, b * 580 / 768), fen, getClass().getResource("IUT.png"), "Campus IUT de Laval", listdelistdepoint, null);
+		pan2 = iut.getMiniature();
+		
+		informatique = new Cartes(new Dimension( a * 650 / 1366, b * 580 / 768), fen, getClass().getResource("informatique.png"), "Département Informatique de l'IUT de Laval", listdelistdepoint, null);
+		pan3 = informatique.getMiniature();
+		
+		
 		Font f0 = new Font("Calibri", Font.PLAIN, a * 80 / 1366);
 		Font f1 = new Font("Calibri", Font.PLAIN, a * 25 / 1366);
 		Font f2 = new Font("Calibri", Font.PLAIN, a * 18 / 1366);
@@ -89,13 +241,13 @@ public class PanelChoixCarteArcade extends JPanel {
 			}
 		});
 		
-		
+	
 		panCartes.setPreferredSize(new Dimension((int)(2*a/3-a*20/1920),(int)(b*500*4/1920)));
 		panCartes.setLayout(new GridLayout(4, 3, a * 40 / 1920, a * 40 / 1920));
-		panCartes.add(boutonattente);
-		panCartes.add(new JButton());
-		panCartes.add(new JButton());
-		panCartes.add(new JButton());
+		panCartes.add(laval.getMiniature());
+		panCartes.add(mayenne.getMiniature());
+		panCartes.add(iut.getMiniature());
+		panCartes.add(informatique.getMiniature());
 		panCartes.add(new JButton());
 		panCartes.add(new JButton());
 		panCartes.add(new JButton());
@@ -105,10 +257,14 @@ public class PanelChoixCarteArcade extends JPanel {
 		panCartes.add(new JButton());
 
 		panCartes.setOpaque(false);
+		
 		scroll = new JScrollPane(panCartes);
+		scroll.setForeground(new Color(255, 255, 255,0));
 		scroll.setBounds(a / 6, (b / 40) * 15, 2*a/3, (b / 2));
 		scroll.setOpaque(false);
+		scroll.setBackground(new Color(255, 255, 255,0));
 		scroll.setBorder(null);
+		scroll.getVerticalScrollBar().setUnitIncrement(16);
 		/* Espace tempo */
 
 		boutonattente.setBounds(a * 525 / 1366, b / 8, 300, 200);
@@ -116,7 +272,7 @@ public class PanelChoixCarteArcade extends JPanel {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				fen.setContentPane(new PanelArcade(fen));
+				fen.setContentPane(new PanelArcade(fen,mayenne));
 				fen.revalidate();
 
 			}
@@ -132,12 +288,11 @@ public class PanelChoixCarteArcade extends JPanel {
 	// fond d'ecran
 	public void paintComponent(Graphics g) {
 		try {
-			Image img = ImageIO.read(new File("investigation.jpg"));
+			Image img = ImageIO.read(getClass().getResource("investigation.jpg")); 
 			g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 }
