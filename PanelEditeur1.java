6f720be5668793a03d3898a91ee62bea03514147
wask8 <jeremy.charrier@hotmail.fr>
2015-06-24 10:04:43 +0200

    Update PanelEditeur1.java

--- PanelEditeur1.java
+++ PanelEditeur1.java
@@ -7,6 +7,8 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.io.File;
 import java.io.IOException;
 
@@ -30,17 +32,16 @@ public class PanelEditeur1 extends JPanel {
 	private JButton boutonAnnu = new JButton(
 			"ANNULER et revenir au menu principal");
 	private JButton boutonVal = new JButton("VALIDER et passer a l'Etape 2");
+	private int a;
+	private int b;
 
 	public PanelEditeur1(final Fenetre fen) {
 		// FENETRE GLOBALE
-		Dimension dim = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-		int a = (int) dim.getWidth();
-		int b = (int) dim.getHeight();
+		 a = (int) fen.getWidth();
+		 b = (int) fen.getHeight();
 
 		this.setPreferredSize(new Dimension(a, b));
-//		this.setTitle("Investigation - Editeur Partie 1");
-//		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-//		this.setLocationRelativeTo(null);
+
 		Font f0 = new Font("Calibri", Font.PLAIN, a*80/1366);
 		Font f1 = new Font("Calibri", Font.PLAIN, a*50/1366);
 		Font f2 = new Font("Calibri", Font.PLAIN, 30);
@@ -141,11 +142,66 @@ public class PanelEditeur1 extends JPanel {
 		
 
 		this.setLayout(null);
+
+
+		fen.getRootPane().addComponentListener(new ComponentListener() {
+			
+			@Override
+			public void componentShown(ComponentEvent e) {
+				
+				
+			}
+			
+			@Override
+			public void componentResized(ComponentEvent e) {
+				
+				Font f0 = new Font("Calibri", Font.PLAIN, a*80/1366);
+				Font f1 = new Font("Calibri", Font.PLAIN, a*50/1366);
+				Font f2 = new Font("Calibri", Font.PLAIN, a*30/1366);
+				Font f3 = new Font("Calibri", Font.PLAIN, a*18/1366);
+				
+				panTitre.setBounds(0, 0, a, (int) (b / 4));
+				titre.setBounds((int)(a*450/1366), (int)(b*20/768),(int)(a*800/1366), (int)(b*130/768));
+				soustitre.setBounds((int)(a*430/1366), (int)(b*100/768), (int)(a*800/1366), (int)(b*130/768));
+				panInstruction.setBounds(0, b/7*2, a, b/4*3);
+				etape.setBounds((int)(a*50/1366),(int)(b*20/768),(int)(a*500/1366),(int)(b*30/768));
+				indications.setBounds((int)(a*150/1366),(int)(b*130/768),(int)(a*800/1366),(int)(b*30/768));
+				zoneimport.setBounds((int)(a*300/1366),(int)(b*230/768),(int)(a*400/1366),(int)(b*30/768));
+				boutonimport.setBounds((int)(a*850/1366),(int)(b*230/768),(int)(a*150/1366),(int)(b*30/768));
+				panBouton.setBounds(0,(int)(b*540/768),(int)(a*1500/1366),(int)(b*250/768));
+				boutonAnnu.setBounds((int)(a*200/1366),(int)(b*50/768),(int)(a*400/1366),(int)(b*50/768));
+				boutonVal.setBounds((int)(a*720/1366),(int)(b*50/768),(int)(a*400/1366),(int)(b*50/768));
+
+				
+				titre.setFont(f0);
+				soustitre.setFont(f1);
+				etape.setFont(f2);
+				indications.setFont(f2);
+				zoneimport.setFont(f2);
+				boutonimport.setFont(f3);
+				boutonAnnu.setFont(f3);
+				boutonVal.setFont(f3);
+				
+			fen.revalidate();
+			}
+
+			@Override
+			public void componentMoved(ComponentEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void componentHidden(ComponentEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	public void paintComponent(Graphics g) {
 		try {
-			Image img = ImageIO.read(new File("investigation.jpg"));
+			Image img = ImageIO.read(getClass().getResource("investigation.jpg")); 
 			g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
 		} catch (IOException e) {
 			e.printStackTrace();
